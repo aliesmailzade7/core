c9de45c689bddd9bf21d131f86906210d3cb0bfa
AliEsmaeilzadeh <AliEsmailzade7@gmail.com>
2023-11-04 09:49:32 +0330

    fix(mapper): fix mapper

--- src/main/java/com/cybercenter/core/mapper/ScopeMapper.java
+++ src/main/java/com/cybercenter/core/mapper/ScopeMapper.java
@@ -3,14 +3,14 @@ package com.cybercenter.core.mapper;
 import com.cybercenter.core.dto.ScopeDTO;
 import com.cybercenter.core.entity.ScopeEntity;
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Mapper
-@Component
+@Mapper(componentModel = "spring")
 public interface ScopeMapper {
     ScopeDTO toDTO(ScopeEntity entity);
+
     List<ScopeDTO> toDTOs(List<ScopeEntity> entity);
+
     ScopeEntity toEntity(ScopeDTO toDTO);
 }
--- src/main/java/com/cybercenter/core/mapper/UserMapper.java
+++ src/main/java/com/cybercenter/core/mapper/UserMapper.java
@@ -1,23 +1,19 @@
 package com.cybercenter.core.mapper;
 
-import com.cybercenter.core.dto.RegisterRequestDTO;
 import com.cybercenter.core.constant.Education;
+import com.cybercenter.core.dto.RegisterRequestDTO;
 import com.cybercenter.core.dto.UserInfoDTO;
 import com.cybercenter.core.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 
-import javax.persistence.PreUpdate;
-
 @Mapper(componentModel = "spring", imports = {Education.class, ObjectUtils.class})
-@Component
 public interface UserMapper {
 
-    @Mapping(target = "education", expression = "java(!ObjectUtils.isEmpty(entity.getEducation()) ? entity.getEducation().getId() : null)" )
-    @Mapping(target = "educationTitle", expression = "java(!ObjectUtils.isEmpty(entity.getEducation()) ? entity.getEducation().getTitle() : null)" )
+    @Mapping(target = "education", expression = "java(!ObjectUtils.isEmpty(entity.getEducation()) ? entity.getEducation().getId() : null)")
+    @Mapping(target = "educationTitle", expression = "java(!ObjectUtils.isEmpty(entity.getEducation()) ? entity.getEducation().getTitle() : null)")
     UserInfoDTO toDTO(User entity);
 
     User ToEntity(RegisterRequestDTO registerRequestDTO);
@@ -27,6 +23,6 @@ public interface UserMapper {
     @Mapping(target = "password", ignore = true)
     @Mapping(target = "roles", ignore = true)
     @Mapping(target = "enable", ignore = true)
-    @Mapping(target = "education", expression = "java(!ObjectUtils.isEmpty(userInfoDTO.getEducation()) ? Education.of(userInfoDTO.getEducation()) : null)" )
+    @Mapping(target = "education", expression = "java(!ObjectUtils.isEmpty(userInfoDTO.getEducation()) ? Education.of(userInfoDTO.getEducation()) : null)")
     void updateUserInfo(@MappingTarget User user, UserInfoDTO userInfoDTO);
 }
